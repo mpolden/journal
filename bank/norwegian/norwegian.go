2ee0aa0b00ab442bfbe50ecdfeb2914e1d19831d
Martin Polden <mpolden@mpolden.no>
2018-07-14 15:37:55 +0200

    Implement Bank Norwegian import

---
+++ bank/norwegian/norwegian.go
@@ -0,0 +1,79 @@
+package norwegian
+
+import (
+	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mpolden/journal/bank"
+	"github.com/pkg/errors"
+	"github.com/tealeg/xlsx"
+)
+
+const (
+	firstHeaderCell   = "TransactionDate"
+	decimalSeparator  = "."
+	thousandSeparator = ","
+)
+
+var replacer = strings.NewReplacer(decimalSeparator, "", thousandSeparator, "")
+
+func parseAmount(s string) (int64, error) {
+	if strings.LastIndex(s, decimalSeparator) == len(s)-2 { // Pad single digit decimal
+		s += "0"
+	}
+	hasDecimals := strings.Contains(s, decimalSeparator)
+	v := replacer.Replace(s)
+	n, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	if !hasDecimals {
+		return n * 100, nil
+	}
+	return n, nil
+}
+
+func Parse(r io.Reader) ([]bank.Transaction, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	f, err := xlsx.OpenBinary(data)
+	if err != nil {
+		return nil, err
+	}
+	if len(f.Sheets) == 0 {
+		return nil, errors.New("xlsx contains 0 sheets")
+	}
+	var ts []bank.Transaction
+	for _, row := range f.Sheets[0].Rows {
+		cells := row.Cells
+		if len(cells) < 7 {
+			continue
+		}
+		if cells[0].String() == firstHeaderCell { // Header row
+			continue
+		}
+		if cells[0].String() == "" { // Empty row
+			continue
+		}
+		time, err := time.Parse("01-02-06", cells[0].String())
+		if err != nil {
+			return nil, errors.Wrapf(err, "invalid date: %q", cells[0].String())
+		}
+		amount, err := parseAmount(cells[6].String())
+		if err != nil {
+			return nil, errors.Wrapf(err, "invalid amount: %q", cells[6].String())
+		}
+		t := bank.Transaction{
+			Time:   time,
+			Text:   cells[1].String(),
+			Amount: amount,
+		}
+		ts = append(ts, t)
+	}
+	return ts, nil
+}
---
+++ bank/norwegian/norwegian_test.go
@@ -0,0 +1,56 @@
+package norwegian
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func date(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+}
+
+func TestParse(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	testFile := filepath.Join(wd, "testdata", "test.xlsx")
+
+	f, err := os.Open(testFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+
+	ts, err := Parse(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var tests = []struct {
+		t      time.Time
+		text   string
+		amount int64
+	}{
+		{date(2017, 2, 1), "Transaction 1", 133700},
+		{date(2017, 3, 10), "Transaction 2", -4233},
+		{date(2017, 4, 20), "Transaction 3", 4233},
+		{date(2017, 5, 20), "Transaction 4", -4230},
+	}
+	if len(ts) != len(tests) {
+		t.Fatalf("want %d records, got %d", len(tests), len(ts))
+	}
+	for i, tt := range tests {
+		if !ts[i].Time.Equal(tt.t) {
+			t.Errorf("#%d: want Time = %s, got %s", i, tt.t, ts[i].Time)
+		}
+		if ts[i].Text != tt.text {
+			t.Errorf("#%d: want Text = %s, got %s", i, tt.text, ts[i].Text)
+		}
+		if ts[i].Amount != tt.amount {
+			t.Errorf("#%d: want Amount = %d, got %d", i, tt.amount, ts[i].Amount)
+		}
+	}
+}
