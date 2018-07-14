044febf524c1983a039f232fb31ce19092d7e7a4
Martin Polden <mpolden@mpolden.no>
2018-07-15 00:34:39 +0200

    Implement persistence

---
+++ sql/sql.go
@@ -0,0 +1,163 @@
+package sql
+
+import (
+	"database/sql"
+	"sync"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
+)
+
+const schema = `
+CREATE TABLE IF NOT EXISTS account (
+  id INTEGER PRIMARY KEY,
+  number TEXT NOT NULL,
+  description TEXT NOT NULL,
+  CONSTRAINT number_unique UNIQUE (number)
+);
+
+CREATE TABLE IF NOT EXISTS record (
+  id INTEGER PRIMARY KEY,
+  account_id INTEGER NOT NULL,
+  time INTEGER NOT NULL,
+  text TEXT NOT NULL,
+  amount INTEGER NOT NULL,
+  FOREIGN KEY(account_id) REFERENCES account(id)
+);
+
+CREATE INDEX IF NOT EXISTS record_time_idx ON record (time);
+`
+
+type client struct {
+	db *sqlx.DB
+	mu sync.RWMutex
+}
+
+type Account struct {
+	Number      string `db:"number"`
+	Description string `db:"description"`
+}
+
+type Record struct {
+	Time   int64  `db:"time"`
+	Text   string `db:"text"`
+	Amount int64  `db:"amount"`
+	Account
+}
+
+func New(filename string) (*client, error) {
+	db, err := sqlx.Connect("sqlite3", filename)
+	if err != nil {
+		return nil, err
+	}
+	// Ensure foreign keys are enabled (defaults to off)
+	if _, err := db.Exec("PRAGMA foreign_keys = ON"); err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(schema); err != nil {
+		return nil, err
+	}
+	return &client{db: db}, nil
+}
+
+func (c *client) AddAccount(account Account) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	tx, err := c.db.Beginx()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	id := 0
+	err = tx.Get(&id, "SELECT id FROM account WHERE number = $1 LIMIT 1", account.Number)
+	if err == sql.ErrNoRows {
+		if _, err := tx.Exec("INSERT INTO account (number, description) VALUES ($1, $2)", account.Number, account.Description); err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+func (c *client) GetAccount(number string) (Account, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	var account Account
+	query := "SELECT number, description FROM account WHERE number = $1 LIMIT 1"
+	if err := c.db.Get(&account, query, number); err != nil {
+		return Account{}, err
+	}
+	return account, nil
+}
+
+func (c *client) AddRecords(account Account, records []Record) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	tx, err := c.db.Beginx()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	accountID := 0
+	if err := tx.Get(&accountID, "SELECT id FROM account WHERE number = $1 LIMIT 1", account.Number); err != nil {
+		return errors.Wrapf(err, "invalid account: %s", account.Number)
+	}
+
+	query := `
+SELECT COUNT(*)
+FROM record
+WHERE account_id = $1 AND time = $2 AND text = $3 AND amount = $4
+LIMIT 1`
+
+	insertQuery := `
+INSERT INTO record (account_id, time, text, amount)
+VALUES ($1, $2, $3, $4)
+`
+
+	for _, r := range records {
+		count := 0
+		if err := tx.Get(&count, query, accountID, r.Time, r.Text, r.Amount); err != nil {
+			return err
+		}
+		if count > 0 {
+			continue
+		}
+		if _, err := tx.Exec(insertQuery, accountID, r.Time, r.Text, r.Amount); err != nil {
+			return err
+		}
+	}
+	return tx.Commit()
+}
+
+func (c *client) SelectRecords(accountNumber string) ([]Record, error) {
+	return c.SelectRecordsBetween(accountNumber, time.Time{}, time.Time{})
+}
+
+func (c *client) SelectRecordsBetween(accountNumber string, since, until time.Time) ([]Record, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	query := `
+SELECT number, time, text, amount FROM record
+INNER JOIN account ON account_id = account.id
+WHERE number = ?`
+	args := []interface{}{accountNumber}
+	if !since.IsZero() {
+		query += " AND time >= ?"
+		args = append(args, since.Unix())
+	}
+	if !until.IsZero() {
+		query += " AND time <= ?"
+		args = append(args, until.Unix())
+	}
+	query += " ORDER BY time DESC"
+	var rs []Record
+	if err := c.db.Select(&rs, query, args...); err != nil {
+		return nil, err
+	}
+	return rs, nil
+}
---
+++ sql/sql_test.go
@@ -0,0 +1,93 @@
+package sql
+
+import (
+	"testing"
+	"time"
+)
+
+func testClient() *client {
+	c, err := New(":memory:")
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
+
+func date(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+}
+
+func TestAddAccount(t *testing.T) {
+	c := testClient()
+	number := "1.33.7"
+	for i := 0; i < 2; i++ {
+		if err := c.AddAccount(Account{Number: number, Description: "Savings"}); err != nil {
+			t.Fatal(err)
+		}
+	}
+	count := 0
+	if err := c.db.Get(&count, "SELECT COUNT(*) FROM account LIMIT 1"); err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Errorf("want %d accounts, got %d", 1, count)
+	}
+	account, err := c.GetAccount(number)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := account.Number, "1.33.7"; got != want {
+		t.Errorf("want Number = %s, got %s", want, got)
+	}
+}
+
+func TestAddRecords(t *testing.T) {
+	c := testClient()
+	account := Account{Number: "1.33.7", Description: "Savings"}
+	if err := c.AddAccount(account); err != nil {
+		t.Fatal(err)
+	}
+	records := []Record{
+		{Time: date(2017, 4, 20).Unix(), Text: "Transaction 4", Amount: 5678},
+		{Time: date(2017, 3, 15).Unix(), Text: "Transaction 3", Amount: 24},
+		{Time: date(2017, 2, 10).Unix(), Text: "Transaction 2", Amount: 1234},
+		{Time: date(2017, 1, 1).Unix(), Text: "Transaction 1", Amount: 42},
+		{Time: date(2017, 1, 1).Unix(), Text: "Transaction 1", Amount: 42}, // Duplicate, ignored
+	}
+	if err := c.AddRecords(account, records); err != nil {
+		t.Fatal(err)
+	}
+	rs, err := c.SelectRecords(account.Number)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(rs), len(records)-1; got != want {
+		t.Errorf("want len = %d, got %d", want, got)
+	}
+	for i, r := range rs {
+		if records[i].Time != r.Time {
+			t.Errorf("want Time = %d, got %d", records[i].Time, r.Time)
+		}
+		if records[i].Text != r.Text {
+			t.Errorf("want Text = %s, got %s", records[i].Text, r.Text)
+		}
+		if records[i].Amount != r.Amount {
+			t.Errorf("want Amount = %d, got %d", records[i].Amount, r.Amount)
+		}
+	}
+	since := date(2017, 2, 10)
+	until := date(2017, 3, 15)
+	rs, err = c.SelectRecordsBetween(account.Number, since, until)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := len(rs), 2; got != want {
+		t.Errorf("want len = %d, got %d", want, got)
+	}
+	if got, want := rs[0].Time, until.Unix(); got != want {
+		t.Errorf("want Time = %d, got %d", want, got)
+	}
+	if got, want := rs[1].Time, since.Unix(); got != want {
+		t.Errorf("want Time = %d, got %d", want, got)
+	}
+}
