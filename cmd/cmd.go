e11d39d0508432c4f3b62a6fec92063ffe405131
Martin Polden <mpolden@mpolden.no>
2018-07-15 18:37:12 +0200

    Implement CLI for importing records

---
+++ cmd/cmd.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mpolden/journal/journal"
+	"github.com/mpolden/journal/record"
+	"github.com/mpolden/journal/record/komplett"
+	"github.com/mpolden/journal/record/norwegian"
+)
+
+type opts struct {
+	Config string `short:"f" long:"config" description:"Config file" value-name:"FILE" default:"~/.journalrc"`
+}
+
+type Import struct {
+	opts
+	Log    *log.Logger
+	Parser string `short:"p" long:"parser" description:"Name of parser to use when importing data" choice:"csv" choice:"komplett" choice:"norwegian" default:"csv"`
+	Dryrun bool   `short:"n" long:"dry-run" description:"Only print what would happen"`
+	Args   struct {
+		Account string `description:"Account number" positional-arg-name:"account-number"`
+		File    string `description:"File containing records to import" positional-arg-name:"import-file"`
+	} `positional-args:"yes" required:"yes"`
+}
+
+func (i *Import) Execute(args []string) error {
+	j, err := journal.FromConfig(i.Config)
+	if err != nil {
+		return err
+	}
+
+	rs, err := i.readRecords()
+	if err != nil {
+		return err
+	}
+
+	return j.Write(i.Args.Account, rs)
+}
+
+func (i *Import) readRecords() ([]record.Record, error) {
+	f, err := os.Open(i.Args.File)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var r record.Reader
+	switch i.Parser {
+	case "csv":
+		r = record.NewReader(f)
+	case "komplett":
+		r = komplett.NewReader(f)
+	case "norwegian":
+		r = norwegian.NewReader(f)
+	default:
+		return nil, fmt.Errorf("invalid parser: %q", i.Parser)
+	}
+	return r.Read()
+}
---
+++ cmd/journal/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+	"github.com/mpolden/journal/cmd"
+)
+
+func main() {
+	p := flags.NewParser(nil, flags.Default)
+	log := log.New(os.Stderr, "journal: ", 0)
+
+	imp := cmd.Import{Log: log}
+	if _, err := p.AddCommand("import", "Import records",
+		"Imports records into the database.", &imp); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := p.Parse(); err != nil {
+		log.Fatal(err)
+	}
+}
