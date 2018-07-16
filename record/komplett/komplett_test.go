cf09c489b50f0c0c951dbaac76297148a6d037cb
Martin Polden <mpolden@mpolden.no>
2018-07-16 14:04:28 +0200

    Fix test names

--- record/komplett/komplett_test.go
+++ record/komplett/komplett_test.go
@@ -11,7 +11,7 @@ func date(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 }
 
-func TestReadFrom(t *testing.T) {
+func TestRead(t *testing.T) {
 	wd, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
