66d6fc2e694ec8c034d6b7d2c95e62294e32e5c1
Martin Polden <mpolden@mpolden.no>
2021-08-09 13:11:00 +0200

    build: Remove deprecated golint

--- go.mod
+++ go.mod
@@ -9,6 +9,5 @@ require (
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/mattn/go-sqlite3 v1.14.6
 	github.com/olekukonko/tablewriter v0.0.4
-	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5
 	honnef.co/go/tools v0.1.2
 )
--- tools.go
+++ tools.go
@@ -2,11 +2,9 @@
 
 // Disable SA5008 because cmd packages has a duplicate "choice" tag
 //go:generate go run honnef.co/go/tools/cmd/staticcheck -checks inherit,-SA5008 ./...
-//go:generate go run golang.org/x/lint/golint ./...
 
 package tools
 
 import (
-	_ "golang.org/x/lint/golint"
 	_ "honnef.co/go/tools/cmd/staticcheck"
 )
