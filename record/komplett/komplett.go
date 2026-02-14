2c9403b907084c192307ba9466008c0244f0a772
Martin Polden <mpolden@mpolden.no>
2026-02-14 15:24:51 +0100

    Revert "komplett: remove obsolete format"
    This reverts commit b1563662ed55121af09b69d5240a4079b3e2c88e.

---
+++ record/komplett/komplett.go
@@ -0,0 +1,109 @@
+package komplett
+
+import (
+	"encoding/json"
+	"io"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/mpolden/journal/record"
+)
+
+const timeLayout = "02.01.2006"
+
+var (
+	separatorPattern = regexp.MustCompile("[.,]")
+	cleanPattern     = regexp.MustCompile(`kr|NOK|"|\s+|\p{Z}+`)
+)
+
+// Reader implements a reader for Komplett-encoded (JSON) records.
+type Reader struct{ rd io.Reader }
+
+type jsonTime time.Time
+
+type jsonAmount int64
+
+type jsonRecord struct {
+	// The JSON from their API keeps shuffling field names. Each number corresponds to a version of the format
+	IsReserved bool       `json:"IsReserved"`
+	Time1      jsonTime   `json:"FormattedPostingDate"`
+	Time2      jsonTime   `json:"TransactionDate"`
+	Amount1    jsonAmount `json:"BillingAmount"`
+	Amount2    jsonAmount `json:"FormattedAmount"`
+	Text1      string     `json:"DisplayDescription"`
+	Text2      string     `json:"MerchantName"`
+	Text3      string     `json:"Description"`
+}
+
+func (t *jsonTime) UnmarshalJSON(data []byte) error {
+	s, err := strconv.Unquote(string(data))
+	if err != nil {
+		return err
+	}
+	tt, err := time.Parse(timeLayout, s)
+	if err != nil {
+		return err
+	}
+	*t = jsonTime(tt)
+	return nil
+}
+
+func (a *jsonAmount) UnmarshalJSON(data []byte) error {
+	text := cleanPattern.ReplaceAllString(string(data), "")
+	parts := separatorPattern.Split(text, -1)
+	firstPart := parts[0]
+	if len(parts) == 2 {
+		firstPart += parts[1]
+		if len(parts[1]) == 1 {
+			firstPart += "0"
+		}
+	} else {
+		firstPart += "00"
+	}
+	n, err := strconv.ParseInt(firstPart, 10, 64)
+	if err != nil {
+		return err
+	}
+	*a = jsonAmount(n)
+	return nil
+}
+
+// NewReader returns a new reader for Komplett-encoded records.
+func NewReader(rd io.Reader) *Reader {
+	return &Reader{rd: rd}
+}
+
+func (r *Reader) Read() ([]record.Record, error) {
+	var jrs []jsonRecord
+	if err := json.NewDecoder(r.rd).Decode(&jrs); err != nil {
+		return nil, err
+	}
+	var rs []record.Record
+	for _, jr := range jrs {
+		if jr.IsReserved {
+			continue
+		}
+		amount := jr.Amount1
+		if amount == 0 {
+			amount = jr.Amount2
+		}
+		txTime := time.Time(jr.Time1)
+		if txTime.IsZero() {
+			txTime = time.Time(jr.Time2)
+		}
+		text := jr.Text1
+		if text == "" {
+			text = jr.Text2
+		}
+		if text == "" {
+			text = jr.Text3
+		}
+		rs = append(rs, record.Record{
+			Time:   txTime,
+			Text:   text,
+			Amount: int64(amount),
+		})
+	}
+	return rs, nil
+}
---
+++ record/komplett/komplett_test.go
@@ -0,0 +1,100 @@
+package komplett
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func date(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+}
+
+func testFile(t *testing.T, name string) *os.File {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	testFile := filepath.Join(wd, "testdata", name)
+
+	f, err := os.Open(testFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return f
+}
+
+func TestLegacyRead(t *testing.T) {
+	f := testFile(t, "legacy.json")
+	defer f.Close()
+
+	r := NewReader(f)
+	rs, err := r.Read()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var tests = []struct {
+		t      time.Time
+		text   string
+		amount int64
+	}{
+		{date(2017, 9, 1), "Innskudd / Ekstra avdrag", 4242},
+		{date(2017, 8, 1), "Innskudd / Ekstra avdrag", 133700},
+		{date(2018, 9, 1), "Varekjøp", -50000},
+		{date(2018, 12, 1), "Varekjøp", -99740},
+	}
+	if len(rs) != len(tests) {
+		t.Fatalf("want %d records, got %d", len(tests), len(rs))
+	}
+	for i, tt := range tests {
+		if !rs[i].Time.Equal(tt.t) {
+			t.Errorf("#%d: want Time = %s, got %s", i, tt.t, rs[i].Time)
+		}
+		if rs[i].Text != tt.text {
+			t.Errorf("#%d: want Text = %s, got %s", i, tt.text, rs[i].Text)
+		}
+		if rs[i].Amount != tt.amount {
+			t.Errorf("#%d: want Amount = %d, got %d", i, tt.amount, rs[i].Amount)
+		}
+	}
+}
+
+func TestRead(t *testing.T) {
+	f := testFile(t, "test.json")
+	defer f.Close()
+
+	r := NewReader(f)
+	rs, err := r.Read()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var tests = []struct {
+		t      time.Time
+		text   string
+		amount int64
+	}{
+		{date(2019, 10, 30), "Varekjøp", -299000},
+		{date(2019, 10, 30), "Uttak av bonus", 6000},
+		{date(2020, 12, 14), "Varekjøp", -9900},
+		{date(2020, 12, 28), "Varekjøp (5,00 EUR / Kurs 10,74000)", -5370},
+		{date(2022, 2, 1), "Varekjøp 2", -29601},
+		{date(2022, 1, 15), "Varekjøp 3", -133701},
+	}
+	if len(rs) != len(tests) {
+		t.Fatalf("want %d records, got %d", len(tests), len(rs))
+	}
+	for i, tt := range tests {
+		if !rs[i].Time.Equal(tt.t) {
+			t.Errorf("#%d: want Time = %s, got %s", i, tt.t, rs[i].Time)
+		}
+		if rs[i].Text != tt.text {
+			t.Errorf("#%d: want Text = %s, got %s", i, tt.text, rs[i].Text)
+		}
+		if rs[i].Amount != tt.amount {
+			t.Errorf("#%d: want Amount = %d, got %d", i, tt.amount, rs[i].Amount)
+		}
+	}
+}
